f43a8ae3968645760e44dc30457e652cba011367
EC2 Default User <ec2-user@ip-172-31-32-162.ap-northeast-1.compute.internal>
2023-08-12 06:01:00 +0000

    Update3-1

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,30 @@
 class BooksController < ApplicationController
+  
+ def new
+   
+   @book = Book.new
+   
+ end
+
+  def create
+    
+  book = Book.new(list_params)
+    
+   book.save
+  
+    redirect_to '/top'
+  end
+  
+  def index
+    
+  end
+  
+  def show
+    
+  end
+  
+  def edit 
+    
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  get 'homes/top' => 'homes#top'
-  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'books/new'
+  post 'books' => 'books#create'
+  get 'books/index'
+  get 'books/show'
+  get 'books/edit'
+  get 'top' => 'homes#top'
+  post 'books' => 'bookers#create'
+
 end
